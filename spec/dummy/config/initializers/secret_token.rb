c0ec803cc868c48af52b0075a1a6ee46c226f214
Mikhail Grachev <work@mgrachev.com>
2015-04-08 17:03:30 +0300

    Update dummy app

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'b6800efe574cc7e5ccaba86e3cd4dc7c3d83f72d3e37bdf21f49e04255caf810a406ba5365aa96050b54f4682ccabff6c67b28f3be7afc5c6b037730cac98766'
